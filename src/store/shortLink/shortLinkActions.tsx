5ef897935a0733ab63000e3701444081db9eacbb
unknown <goafucheng@gmail.com>
2023-10-25 09:34:36 +0400

    add the shortlinks

--- src/store/shortLink/shortLinkActions.tsx
+++ src/store/shortLink/shortLinkActions.tsx
@@ -21,7 +21,6 @@ import {
   deleteLink,
   getDocumentDetails,
   shortUrlUpdate,
-  deleteShortLink,
   deleteMultiLinks,
 } from "./shortLinkService";
 import Message from "../../modules/shared/Message";
